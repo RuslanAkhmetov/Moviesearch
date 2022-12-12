aa243b0febb16c137a5f5e269a0c6fda7fee4543
RuslanAkhmetov <akhmrusl@gmail.com>
2022-12-12 21:50:21 +0300

    first

---
+++ app/src/main/java/com/geekbrain/androidwithkotlin/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.geekbrain.androidwithkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.geekbrain.androidwithkotlin.databinding.ActivityMainBinding
+import com.geekbrain.androidwithkotlin.ui.main.MainFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setSupportActionBar(binding.toolbar);
+        binding.bottomNavigation.setOnNavigationItemReselectedListener {
+            when (it.itemId) {
+                R.id.action_main -> {
+
+                }
+                R.id.action_category -> {
+
+                }
+                R.id.action_favorite -> {
+
+                }
+            }
+            true
+        }
+
+    if (savedInstanceState == null)
+    {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, MainFragment.newInstance())
+            .commitNow()
+    }
+}
+
+override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    menuInflater.inflate(R.menu.user_info_menu, menu);
+    return true
+}
+
+override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+    R.id.action_search -> {
+        true
+    }
+
+    R.id.action_sort -> {
+        true
+    }
+
+    R.id.action_settings -> {
+        true
+    }
+
+    else -> {
+        super.onOptionsItemSelected(item)
+    }
+}
+
+
+
+}
